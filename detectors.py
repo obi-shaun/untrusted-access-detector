559ebeb25188c964e9e660fcb7b6ca63342a03e7
Bubbles <bubbles@Bubbless-MacBook-Pro.local>
2022-02-20 20:29:34 -0500

    Checking the statement effect equals 'Allow'

--- detectors.py
+++ detectors.py
@@ -5,8 +5,7 @@ import re
 
 class IAMRoleDetector:
 
-	def __init__(self, trusted_accounts=None, trusted_orgs=None):
-		#TODO: Add support for org ids and lists of accounts
+	def __init__(self):
 		self.account = boto3.client('sts').get_caller_identity().get('Account')
 
 
@@ -21,15 +20,10 @@ class IAMRoleDetector:
 			policy = role['AssumeRolePolicyDocument']
 			untrusted_principals= []
 
-			for statement in policy['Statement']:
-				try:
-					self._collect_untrusted_principals(json.dumps(statement['Principal']['AWS']), untrusted_principals)
-				except KeyError as e:
-					continue
+			self._collect_untrusted_principals(policy, untrusted_principals)
 
 			if untrusted_principals:
 				result_obj = {
-					'role_name': role['RoleName'],
 					'arn': role['Arn'],
 					'untrusted_principals': untrusted_principals
 				}
@@ -38,15 +32,20 @@ class IAMRoleDetector:
 		return results
 
 
-	def _collect_untrusted_principals(self, principal_obj, untrusted_principals):
-		principal_obj = json.loads(principal_obj)
+	def _collect_untrusted_principals(self, policy, untrusted_principals):
+		for statement in policy['Statement']:
+			if statement['Effect'] == 'Allow':
 
-		if isinstance(principal_obj, list):
-			for principal in principal_obj:
-				if self.account not in str(principal) and str(principal) not in untrusted_principals:
-					untrusted_principals.append(principal)
-		elif self.account not in str(principal_obj) and str(principal_obj) not in untrusted_principals:
-			untrusted_principals.append(principal_obj)
+				try:
+					principal_obj = json.loads(json.dumps(statement['Principal']['AWS']))
 
+					if isinstance(principal_obj, list):
+						for principal in principal_obj:
+							if self.account not in str(principal) and str(principal) not in untrusted_principals:
+								untrusted_principals.append(principal)
 
+					elif self.account not in str(principal_obj) and str(principal_obj) not in untrusted_principals:
+						untrusted_principals.append(principal_obj)
 
+				except KeyError as e:
+					continue
